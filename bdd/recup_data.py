bc3b2ec1bb29796bbfbed66859a36f15782fd359
BastienChicot <88327915+BastienChicot@users.noreply.github.com>
2023-01-13 21:54:40 +0100

    modif des données de ml avec test de matrice de corrélation et analyse des variables. + ajout d'une variable repos pour les data de saison

--- bdd/recup_data.py
+++ bdd/recup_data.py
@@ -50,89 +50,95 @@ def recup_values(annee):
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from datetime import datetime
 import os
 
 def data_fbref(saison):
     
     os.chdir("bdd")
     
-    # saison = "2021-2022"
+    liste_saison = ["2019-2020","2020-2021"]
     
     codes = pd.read_csv("data/code_seriea.txt", sep = ";")
     values = pd.read_csv("data/value_tm.txt", sep=";")
-    
     liste_df = []
-    
-    for i in tqdm(codes.index):
-        url_stat = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/"+str(codes["code"][i])+"-Stats"
-        url_shoot = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/matchlogs/all_comps/shooting/"+str(codes["code"][i])+"-Match-Logs-All-Competitions" 
-        url_passe = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/matchlogs/all_comps/passing/"+str(codes["code"][i])+"-Match-Logs-All-Competitions" 
-        #♦url_def = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/matchlogs/all_comps/defense/"+str(codes["code"][i])+"-Match-Logs-All-Competitions" 
-        url_misc = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/matchlogs/all_comps/misc/"+str(codes["code"][i])+"-Match-Logs-All-Competitions" 
-        
-        ##STAT GENERALES
-        r = requests.get(url_stat)
-        r_html = r.text
-        soup = BeautifulSoup(r_html,'html.parser')
-    
-        table=soup.find_all('table', {'id' : 'matchlogs_for'})
-    
-        tab_temp = pd.read_html(str(table[0]))[0]
-        tab_temp["team"] = codes["code"][i]
-        tab_temp["key"] = tab_temp["Date"]+tab_temp["Time"]+tab_temp["Comp"]+tab_temp["Round"]+tab_temp["Venue"]+tab_temp["Result"]+tab_temp["Opponent"]+tab_temp["team"]
-    
-        ##STAT TIRS
-        r_shoot = requests.get(url_shoot)
-        r_html_shoot = r_shoot.text
-        soup_shoot = BeautifulSoup(r_html_shoot,'html.parser')
-    
-        tab2=soup_shoot.find_all('table', {'id' : 'matchlogs_for'})
-    
-        tab_shoot = pd.read_html(str(tab2[0]))[0]
-        tab_shoot.columns = tab_shoot.columns.droplevel(0)
-        tab_shoot["team"] = codes["code"][i]
-        tab_shoot["key"] = tab_shoot["Date"]+tab_shoot["Time"]+tab_shoot["Comp"]+tab_shoot["Round"]+tab_shoot["Venue"]+tab_shoot["Result"]+tab_shoot["Opponent"]+tab_shoot["team"]
-        tab_shoot = tab_shoot[["key","Sh","SoT","Dist","FK","PK"]]
-    
-        ##STAT PASSES
-        r_pass = requests.get(url_passe)
-        r_html_pass = r_pass.text
-        soup_pass = BeautifulSoup(r_html_pass,'html.parser')
-    
-        tab3=soup_pass.find_all('table', {'id' : 'matchlogs_for'})
-    
-        tab_pass = pd.read_html(str(tab3[0]))[0]
-        tab_pass.columns = tab_pass.columns.droplevel(0)
-        tab_pass["team"] = codes["code"][i]
-        tab_pass["key"] = tab_pass["Date"]+tab_pass["Time"]+tab_pass["Comp"]+tab_pass["Round"]+tab_pass["Venue"]+tab_pass["Result"]+tab_pass["Opponent"]+tab_pass["team"]
-        tab_pass = tab_pass[["key","Att","Cmp%"]]
-        ##VERIFIER LA SORTIE DE LA LIGNE CI DESSOUS:
-        tab_pass = tab_pass.iloc[:, [0,1,5]]
-    
-        ##STAT MISC
-        r_misc = requests.get(url_misc)
-        r_html_misc = r_misc.text
-        soup_misc = BeautifulSoup(r_html_misc,'html.parser')
-    
-        tab4=soup_misc.find_all('table', {'id' : 'matchlogs_for'})
-    
-        tab_misc = pd.read_html(str(tab4[0]))[0]
-        tab_misc.columns = tab_misc.columns.droplevel(0)
-        tab_misc["team"] = codes["code"][i]
-        tab_misc["key"] = tab_misc["Date"]+tab_misc["Time"]+tab_misc["Comp"]+tab_misc["Round"]+tab_misc["Venue"]+tab_misc["Result"]+tab_misc["Opponent"]+tab_misc["team"]
-        tab_misc = tab_misc[["key","CrdY","CrdR","2CrdY","Fls","Off","Int"]]
-        
-        tab_temp = tab_temp.merge(tab_shoot, how="left",on="key")
-        tab_temp = tab_temp.merge(tab_pass, how="left",on="key")
-        tab_temp = tab_temp.merge(tab_misc, how="left",on="key")
-        
-        tm_data = values.loc[values["team"] == tab_temp["team"][0]]
-        tm_data = tm_data[["team","age","value"]]    
-        
-        tab_temp = tab_temp.merge(tm_data, how="left",on="team")
-        
-        liste_df.append(tab_temp)
-        
+
+    for saison in liste_saison:
+        for i in tqdm(codes.index):
+                url_stat = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/"+str(codes["code"][i])+"-Stats"
+                url_shoot = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/matchlogs/all_comps/shooting/"+str(codes["code"][i])+"-Match-Logs-All-Competitions" 
+                url_passe = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/matchlogs/all_comps/passing/"+str(codes["code"][i])+"-Match-Logs-All-Competitions" 
+                #♦url_def = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/matchlogs/all_comps/defense/"+str(codes["code"][i])+"-Match-Logs-All-Competitions" 
+                url_misc = "https://fbref.com/en/squads/"+str(codes["code_url"][i])+"/"+str(saison)+"/matchlogs/all_comps/misc/"+str(codes["code"][i])+"-Match-Logs-All-Competitions" 
+                
+                ##STAT GENERALES
+                r = requests.get(url_stat)
+                r_html = r.text
+                soup = BeautifulSoup(r_html,'html.parser')
+                table=soup.find_all('table', {'id' : 'matchlogs_for'})
+            
+                tab_temp = pd.read_html(str(table[0]))[0]
+                tab_temp["team"] = codes["code"][i]
+                tab_temp["key"] = tab_temp["Date"]+tab_temp["Time"]+tab_temp["Comp"]+tab_temp["Round"]+tab_temp["Venue"]+tab_temp["Result"]+tab_temp["Opponent"]+tab_temp["team"]
+            
+                ##STAT TIRS
+                r_shoot = requests.get(url_shoot)
+                r_html_shoot = r_shoot.text
+                soup_shoot = BeautifulSoup(r_html_shoot,'html.parser')
+            
+                tab2=soup_shoot.find_all('table', {'id' : 'matchlogs_for'})
+            
+                tab_shoot = pd.read_html(str(tab2[0]))[0]
+                tab_shoot.columns = tab_shoot.columns.droplevel(0)
+                tab_shoot["team"] = codes["code"][i]
+                tab_shoot["key"] = tab_shoot["Date"]+tab_shoot["Time"]+tab_shoot["Comp"]+tab_shoot["Round"]+tab_shoot["Venue"]+tab_shoot["Result"]+tab_shoot["Opponent"]+tab_shoot["team"]
+                tab_shoot = tab_shoot[["key","Sh","SoT","Dist","FK","PK"]]
+            
+                ##STAT PASSES
+                r_pass = requests.get(url_passe)
+                r_html_pass = r_pass.text
+                soup_pass = BeautifulSoup(r_html_pass,'html.parser')
+            
+                tab3=soup_pass.find_all('table', {'id' : 'matchlogs_for'})
+            
+                tab_pass = pd.read_html(str(tab3[0]))[0]
+                tab_pass.columns = tab_pass.columns.droplevel(0)
+                tab_pass["team"] = codes["code"][i]
+                tab_pass["key"] = tab_pass["Date"]+tab_pass["Time"]+tab_pass["Comp"]+tab_pass["Round"]+tab_pass["Venue"]+tab_pass["Result"]+tab_pass["Opponent"]+tab_pass["team"]
+                tab_pass = tab_pass[["key","Att","Cmp%"]]
+                ##VERIFIER LA SORTIE DE LA LIGNE CI DESSOUS:
+                tab_pass = tab_pass.iloc[:, [0,1,5]]
+            
+                ##STAT MISC
+                r_misc = requests.get(url_misc)
+                r_html_misc = r_misc.text
+                soup_misc = BeautifulSoup(r_html_misc,'html.parser')
+            
+                tab4=soup_misc.find_all('table', {'id' : 'matchlogs_for'})
+            
+                tab_misc = pd.read_html(str(tab4[0]))[0]
+                tab_misc.columns = tab_misc.columns.droplevel(0)
+                tab_misc["team"] = codes["code"][i]
+                tab_misc["key"] = tab_misc["Date"]+tab_misc["Time"]+tab_misc["Comp"]+tab_misc["Round"]+tab_misc["Venue"]+tab_misc["Result"]+tab_misc["Opponent"]+tab_misc["team"]
+                tab_misc = tab_misc[["key","CrdY","CrdR","2CrdY","Fls","Off","Int"]]
+                
+                tab_temp = tab_temp.merge(tab_shoot, how="left",on="key")
+                tab_temp = tab_temp.merge(tab_pass, how="left",on="key")
+                tab_temp = tab_temp.merge(tab_misc, how="left",on="key")
+                
+                tm_data = values.loc[values["team"] == tab_temp["team"][0]]
+                tm_data = tm_data[["team","age","value"]]    
+                
+                tab_temp = tab_temp.merge(tm_data, how="left",on="team")
+                
+                tab_temp["repos"] = 10
+                
+                for j in range(1,len(tab_temp)):
+                    tab_temp["repos"][j] = abs((datetime.strptime(tab_temp["Date"][j], "%Y-%m-%d") - datetime.strptime(tab_temp["Date"][j-1], "%Y-%m-%d")).days)
+                    
+                liste_df.append(tab_temp)
+                
+
     data=pd.concat(liste_df)
     
     data.to_csv("data/data_"+str(saison)+".csv",sep=";")
--- bdd/traitement.py
+++ bdd/traitement.py
@@ -6,7 +6,7 @@ Created on Thu Jan 12 20:35:05 2023
 """
 
 import pandas as pd
-import re
+import numpy as np
 
 #•IMPORT DU FICHIER
 data = pd.read_csv("bdd/data/data_2021-2022.csv", sep= ";", index_col = 0)
@@ -30,10 +30,27 @@ data = data.merge(opp, how="left", on=["opp_key"])
 
 ##CREATION DE LA BASE DE ML AVEC MISE EN FORME DES VARIABLES
 data_ml = data[["Result","Date","Venue","Poss","age","Formation","SoT","Dist","Cmp%","Int",
-                "Fls","value","opp_Formation","opp_value"]]
+                "Fls","value","repos","opp_Formation","opp_value"]]
 
 data_ml['Mois'] = pd.Series(data_ml["Date"].str[5:7])
+
+data_ml['victoire'] = np.where(data_ml['Result']=='W', 1, 0)
+data_ml['home'] = np.where(data_ml['Venue']=='Home', 1, 0)
+data_ml["diff_value"] = data_ml["value"]-data_ml["opp_value"]
+data_ml["nb_def"] = pd.Series(data_ml["Formation"].str[:1])
+data_ml["nb_att"] = pd.Series(data_ml["Formation"].str[-1])
+
+data_ml["nb_def"] = pd.to_numeric(data_ml['nb_def'], errors='coerce').convert_dtypes()
+data_ml["nb_att"] = pd.to_numeric(data_ml['nb_att'], errors='coerce').convert_dtypes()
+
+data_ml["nb_milieu"] = 10 - (data_ml["nb_def"]+data_ml["nb_att"])
 # data_ml.Formation.value_counts()
 
+corr_table = data_ml.corr()
+
+data_ml.columns
+
+data_ml = data_ml[["Result","Venue","Poss","age","Formation","SoT","Dist","Cmp%",
+                   "Int","Fls","diff_value","repos","opp_Formation","Mois"]]
 #EXPORT
 data_ml.to_csv("bdd/data/data_ml.csv",sep=";")
